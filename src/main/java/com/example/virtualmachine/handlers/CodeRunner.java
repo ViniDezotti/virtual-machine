e67546d94831adf3ff830d1b78880cc27edd0b4d
Victor Manoel Rodrigues Guilherme <v.guilherme@samsung.com>
2023-12-02 22:31:37 -0300

    implementando as funções de pilha

--- src/main/java/com/example/virtualmachine/handlers/CodeRunner.java
+++ src/main/java/com/example/virtualmachine/handlers/CodeRunner.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
-import java.util.Scanner;
+import java.util.Objects;
 import java.util.Stack;
 
 public class CodeRunner {
@@ -17,10 +17,11 @@ public class CodeRunner {
     public CodeRunner(String objFile) {
         this.code = saveObjFile(objFile);
         this.memory = new Stack<>();
+        this.auxMemory = new Stack<>();
         this.stackPointer = -1;
         this.programCounter = 0;
 
-
+        execute();
     }
 
     private List<String> saveObjFile(String objFile) {
@@ -37,121 +38,215 @@ public class CodeRunner {
     }
 
     public void execute() {
-        String label;
-        String instruction;
-        String arg1;
-        String arg2;
+        String label = "";
+        String instruction = "";
+        String arg1 = "";
+        String arg2 = "";
 
-        for (int i = 0; i < code.size(); i++) {
-            label = code.get(i).substring(0, 3).strip();
-            instruction = code.get(i).substring(4, 11).strip();
-            arg1 = code.get(i).substring(12, 15).strip();
-            arg2 = code.get(i).substring(16, 19).strip();
+        for (String s : code) {
+            try {
+                label = s.substring(0, 3).strip();
+                instruction = s.substring(4, 11).strip();
+                arg1 = s.substring(12, 15).strip();
+                arg2 = s.substring(16, 19).strip();
+            } catch (StringIndexOutOfBoundsException ex) {
 
-            if (!label.isEmpty()) {
+            }
+
+            if (label.isEmpty()) {
                 switch (instruction) {
-                    case "LDC", "LDV" -> {
+                    case "LDC" -> {
                         stackPointer++;
                         memory.push(arg1);
                     }
+                    case "LDV" -> {
+                        String value;
+
+                        while (!memory.isEmpty()) {
+                            if (memory.size() - 1 == Integer.parseInt(arg1)) {
+                                value = memory.pop();
+                                while (!auxMemory.isEmpty()) {
+                                    memory.push(auxMemory.pop());
+                                }
+                                memory.push(value);
+                                break;
+                            }
+                            auxMemory.push(memory.pop());
+                        }
+                    }
                     case "ADD" -> {
-                        int value1 = Integer.parseInt(memory.pop());
-                        int value2 = Integer.parseInt(memory.pop());
-                        memory.push(String.valueOf(value2 + value1));
-                        stackPointer--;
+                        resolveArithmetic("+");
                     }
                     case "SUB" -> {
-                        int value1 = Integer.parseInt(memory.pop());
-                        int value2 = Integer.parseInt(memory.pop());
-                        memory.push(String.valueOf(value2 - value1));
-                        stackPointer--;
+                        resolveArithmetic("-");
                     }
                     case "MULT" -> {
-                        int value1 = Integer.parseInt(memory.pop());
-                        int value2 = Integer.parseInt(memory.pop());
-                        memory.push(String.valueOf(value2 * value1));
-                        stackPointer--;
+                        resolveArithmetic("*");
                     }
                     case "DIVI" -> {
-                        int value1 = Integer.parseInt(memory.pop());
-                        int value2 = Integer.parseInt(memory.pop());
-                        memory.push(String.valueOf(value2 / value1));
-                        stackPointer--;
+                        resolveArithmetic("/");
+                    }
+                    case "AND" -> {
+                        resolveArithmetic("&");
+                    }
+                    case "OR" -> {
+                        resolveArithmetic("|");
                     }
                     case "INV" -> {
                         int value = Integer.parseInt(memory.pop());
                         memory.push(String.valueOf(-value));
-                    }
-                    case "AND" -> {
-                        int value1 = Integer.parseInt(memory.pop());
-                        int value2 = Integer.parseInt(memory.pop());
-                        memory.push(String.valueOf(value2 & value1));
-                        stackPointer--;
-
-                    }
-                    case "OR" -> {
-                        int value1 = Integer.parseInt(memory.pop());
-                        int value2 = Integer.parseInt(memory.pop());
-                        memory.push(String.valueOf(value2 | value1));
-                        stackPointer--;
                     }
                     case "NEG" -> {
                         int value = Integer.parseInt(memory.pop());
                         memory.push(String.valueOf(1 - value));
                     }
                     case "CME" -> {
-                        int value1 = Integer.parseInt(memory.pop());
-                        int value2 = Integer.parseInt(memory.pop());
-                        if (value2 < value1) memory.push("1");
-                        else memory.push("0");
-                        stackPointer--;
+                        resolveExpression("<");
                     }
                     case "CMA" -> {
-                        int value1 = Integer.parseInt(memory.pop());
-                        int value2 = Integer.parseInt(memory.pop());
-                        if (value2 > value1) memory.push("1");
-                        else memory.push("0");
-                        stackPointer--;
+                        resolveExpression(">");
                     }
                     case "CEQ" -> {
-                        int value1 = Integer.parseInt(memory.pop());
-                        int value2 = Integer.parseInt(memory.pop());
-                        if (value2 == value1) memory.push("1");
-                        else memory.push("0");
-                        stackPointer--;
+                        resolveExpression("==");
                     }
                     case "CDIF" -> {
-                        int value1 = Integer.parseInt(memory.pop());
-                        int value2 = Integer.parseInt(memory.pop());
-                        if (value2 != value1) memory.push("1");
-                        else memory.push("0");
-                        stackPointer--;
+                        resolveExpression("!=");
                     }
                     case "CMEQ" -> {
-                        int value1 = Integer.parseInt(memory.pop());
-                        int value2 = Integer.parseInt(memory.pop());
-                        if (value2 <= value1) memory.push("1");
-                        else memory.push("0");
-                        stackPointer--;
+                        resolveExpression("<=");
                     }
                     case "CMAQ" -> {
-                        int value1 = Integer.parseInt(memory.pop());
-                        int value2 = Integer.parseInt(memory.pop());
-                        if (value2 >= value1) memory.push("1");
-                        else memory.push("0");
+                        resolveExpression(">=");
+                    }
+                    case "STR" -> {
+                        String value = memory.pop();
+
+                        while (!memory.isEmpty()) {
+                            if (memory.size() - 1 == Integer.parseInt(arg1)) {
+                                memory.pop();
+                                memory.push(value);
+                                while (!auxMemory.isEmpty()) {
+                                    memory.push(auxMemory.pop());
+                                }
+                                break;
+                            }
+                            auxMemory.push(memory.pop());
+                        }
+                    }
+                    case "JMP" -> {
+                        programCounter = Integer.parseInt(arg1);
+                    }
+                    case "JMPF" -> {
+                        if (Objects.equals(memory.pop(), "0")) {
+                            programCounter = Integer.parseInt(arg1);
+                        } else stackPointer++;
+                    }
+                    case "NULL" -> {
+
+                    }
+                    case "RD" -> {
+                        stackPointer++;
+                        //TODO memory.push(entrada)
+                    }
+                    case "PRN" -> {
                         stackPointer--;
+                        //TODO imprimir(memory.pop);
+                    }
+                    case "START" -> {
+                        this.stackPointer = -1;
+                    }
+                    case "ALLOC" -> {
+                        stackPointer += Integer.parseInt(arg1);
+
+                        int cont = 0;
+                        while (cont < Integer.parseInt(arg2)) {
+                            memory.push("");
+                            cont++;
+                        }
+                    }
+                    case "DALLOC" -> {
+                        stackPointer -= Integer.parseInt(arg1);
+                        while (!memory.isEmpty()) {
+                            if (memory.size() - 1 == Integer.parseInt(arg1)) {
+                                int index = 0;
+                                while (index < Integer.parseInt(arg1)) {
+                                    auxMemory.pop();
+                                    index++;
+                                }
+                                while (!auxMemory.isEmpty()) {
+                                    memory.push(auxMemory.pop());
+                                }
+                            }
+                            auxMemory.push(memory.pop());
+                        }
                     }
                 }
             }
+            System.out.println(memory);
         }
     }
 
+    private void resolveExpression(String s) {
+        int value1 = Integer.parseInt(memory.pop());
+        int value2 = Integer.parseInt(memory.pop());
+
+        switch (s) {
+            case "<" -> {
+                if (value2 < value1) memory.push("1");
+                else memory.push("0");
+            }
+            case ">" -> {
+                if (value2 > value1) memory.push("1");
+                else memory.push("0");
+            }
+            case "==" -> {
+                if (value2 == value1) memory.push("1");
+                else memory.push("0");
+            }
+            case "!=" -> {
+                if (value2 != value1) memory.push("1");
+                else memory.push("0");
+            }
+            case "<=" -> {
+                if (value2 <= value1) memory.push("1");
+                else memory.push("0");
+            }
+            case ">=" -> {
+                if (value2 >= value1) memory.push("1");
+                else memory.push("0");
+            }
+        }
+
+        stackPointer--;
+    }
+
+    private void resolveArithmetic(String s) {
+        int value1 = Integer.parseInt(memory.pop());
+        int value2 = Integer.parseInt(memory.pop());
+
+        switch (s) {
+            case "+" -> {
+                memory.push(String.valueOf(value2 + value1));
+            }
+            case "-" -> {
+                memory.push(String.valueOf(value2 - value1));
+            }
+            case "*" -> {
+                memory.push(String.valueOf(value2 * value1));
+            }
+            case "/" -> {
+                memory.push(String.valueOf(value2 / value1));
+            }
+            case "&" -> {
+                memory.push(String.valueOf(value2 & value1));
+            }
+            case "|" -> {
+                memory.push(String.valueOf(value2 | value1));
+            }
+        }
+        stackPointer--;
+    }
 
     public void changArgForLine(String arg) {
 
-    }
-
-//    public
-
-
-}
+    }}
